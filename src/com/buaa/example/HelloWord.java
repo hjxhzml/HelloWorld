e2dc4ff60171e6e04555273bf0986887a8c50494
hjxhzml <1726283687@qq.com>
2019-02-24 09:23:06 +0800

    HelloWord提交

---
+++ src/com/buaa/example/HelloWord.java
@@ -0,0 +1,12 @@
+package com.buaa.example;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Thank you for choosing JAVA");
+
+	}
+
+}
