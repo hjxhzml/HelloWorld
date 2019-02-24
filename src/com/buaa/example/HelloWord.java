c2d84ad9b5431d09f1d34f44405b473ac9f0637b
hjxhzml <1726283687@qq.com>
2019-02-24 10:09:21 +0800

    最后的Hello Worlk

--- src/com/buaa/example/HelloWord.java
+++ src/com/buaa/example/HelloWord.java
@@ -1,10 +1,11 @@
 package com.buaa.example;
 
-
 public class HelloWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 10;
+		System.out.println("i="+i);
 		System.out.println("Hello World");
 		System.out.println("Thank you for choosing JAVA");
 
